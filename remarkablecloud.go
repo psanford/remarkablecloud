c5f6dab8e6efa742eee8ab8f147652446248b155
Peter Sanford <psanford@sanford.io>
2024-09-22 13:11:00 -0700

    Move fs code to its own file

--- remarkablecloud.go
+++ remarkablecloud.go
@@ -14,12 +14,9 @@ import (
 	"log"
 	"net/http"
 	"net/http/httputil"
-	"path"
 	"strconv"
 	"strings"
 	"sync"
-
-	"github.com/psanford/memfs"
 )
 
 var (
@@ -378,58 +375,6 @@ type PutResult struct {
 	OldRootHash string
 	NewRootHash string
 	DocID       string
-}
-
-func (c *Client) fsSnapshotFromList(items []Item) (fs.FS, error) {
-	seen := make(map[string]Item)
-	paths := make(map[string]string)
-	rootFS := memfs.New()
-
-	prevSeen := len(seen)
-	for len(seen) < len(items) {
-		for _, item := range items {
-			if _, exists := seen[item.ID]; exists {
-				continue
-			}
-			if item.Parent == "trash" {
-				seen[item.ID] = item
-				continue
-			}
-			if _, parentExists := seen[item.Parent]; parentExists || item.Parent == "" {
-				pp := item.Name
-				if item.Parent != "" {
-					pp = path.Join(paths[item.Parent], item.Name)
-				}
-
-				paths[item.ID] = pp
-
-				if item.Type == collectionType {
-					rootFS.MkdirAll(pp, 0777)
-					data, _ := json.Marshal(item)
-					err := rootFS.WriteFile(path.Join(pp, ".meta"), data, 0777)
-					if err != nil {
-						panic(err)
-					}
-				} else {
-					data, _ := json.Marshal(item)
-					err := rootFS.WriteFile(pp, data, 0777)
-					if err != nil {
-						panic(err)
-					}
-				}
-
-				seen[item.ID] = item
-			}
-		}
-
-		if len(seen) < len(items) && prevSeen == len(seen) {
-			return nil, fmt.Errorf("failed to make progress building fs tree %d %d %d", prevSeen, len(seen), len(items))
-
-		}
-		prevSeen = len(seen)
-	}
-
-	return rootFS, nil
 }
 
 func (c *Client) FSSnapshot() (fs.FS, error) {
